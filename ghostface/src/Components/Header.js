3a0e6236d078f033e1f129bd482d7febedf3a693
Brandon May <49825791+battleb2014@users.noreply.github.com>
2020-07-15 22:11:13 -0400

    Setup
    Installed dependencies and set up component files

---
+++ ghostface/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'; //Imports react capabilities
+
+//Arrow function to be called in App.js
+const Header = () => {
+    return (
+        <div className='header'>
+            {/*Html syntax goes between the div tags*/}
+
+        </div>
+    );
+}
+
+export default Header; //Exports the Header component (function) to be able to be imported later
