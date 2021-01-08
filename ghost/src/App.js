8602d8dda11da843ef9d28e90b214e98006e9721
Brandon <battleb2014@gmail.com>
2021-01-07 22:07:14 -0500

    Header build

--- ghost/src/App.js
+++ ghost/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './Components/Header';
 
 function App() {
   return (
---
+++ ghost/src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <img src='../Images/dgf_header_pic.jpg' alt='Banner'></img>
+            <div className='buttons'>
+                <button>sdfg</button>
+                <button>sdfhgdfsg</button>
+                <button>dsfg</button>
+                <button>dsfg</button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
