a29ca8e5db48bc595854cb60152a247b57d91d25
Brandon <battleb2014@gmail.com>
2021-01-07 22:53:16 -0500

    Header styling

--- ghost/src/App.js
+++ ghost/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import './Components/Header';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
--- ghost/src/Components/Header.js
+++ ghost/src/Components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Images/dgf_header_pic.jpg';
 
 const Header = () => {
     return (
-        <div>
-            <img src='../Images/dgf_header_pic.jpg' alt='Banner'></img>
+        <div className='header'>
+            <img src={Banner} alt='Banner' />
             <div className='buttons'>
                 <button>sdfg</button>
                 <button>sdfhgdfsg</button>
